bd00c46bd3ed0fc82b22fdfc7baece997fa67646
manjaka andriantsoa <163170414+Manjaka-dev@users.noreply.github.com>
2025-09-23 22:09:15 +0300

    feat: capture de l'url et print dans les logs

---
+++ src/main/java/webframe/core/DispatcherServlet.java
@@ -0,0 +1,21 @@
+package webframe.core;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+public class DispatcherServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String path = req.getPathInfo();
+        if (path !=null) {
+            System.out.println(path);
+        }else {
+            System.err.println("pas de path");
+        }
+    }
+}
